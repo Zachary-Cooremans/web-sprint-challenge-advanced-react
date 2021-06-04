7d4cbd00163118864b7211064cd23a3ef52dcdea
Zachary Cooremans <zcooremans@gmail.com>
2021-06-04 12:15:15 -0500

    MVP

--- src/components/CheckoutForm.test.js
+++ src/components/CheckoutForm.test.js
@@ -1,9 +1,40 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
+import userEvent from '@testing-library/user-event'
 import CheckoutForm from "./CheckoutForm";
+
+
+test("renders without error", () => {
+    render(<CheckoutForm />)
+})
 
 // Write up the two tests here and make sure they are testing what the title shows
 
-test("form header renders", () => {});
+test("form header renders", () => {
+    render(<CheckoutForm />);
+    const header = screen.queryByText(/checkout form/i);
+    expect(header).toBeInTheDocument();
+});
 
-test("form shows success message on submit with form details", () => {});
+test("form shows success message on submit with form details", () => {
+    render (<CheckoutForm />);
+    
+    const firstName = screen.getByLabelText("First Name:");
+    const lastName = screen.getByLabelText("Last Name:");
+    const address = screen.getByLabelText("Address:");
+    const city = screen.getByLabelText("City:");
+    const state = screen.getByLabelText("State:");
+    const zip = screen.getByLabelText("Zip:");
+    const button = screen.getByRole("button");
+
+    userEvent.type(firstName, "Zachary");
+    userEvent.type(lastName, "Cooremans");
+    userEvent.type(address, "1234 Some Street");
+    userEvent.type(city, "Round Rock");
+    userEvent.type(state, "Texas");
+    userEvent.type(zip, "12345");
+    userEvent.click(button);
+
+    const success = screen.getByTestId(/successmessage/i);
+    expect(success).toBeInTheDocument();
+});
--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -3,6 +3,9 @@ import axios from "axios";
 
 export default class PlantList extends Component {
   // add state with a property called "plants" - initialize as an empty array
+  state = {
+    plants: []
+  }
 
   // when the component mounts:
   //   - fetch data from the server endpoint - http://localhost:3333/plants
@@ -10,9 +13,13 @@ export default class PlantList extends Component {
 
   /*********  DON'T CHANGE ANYTHING IN THE RENDER FUNCTION *********/
   componentDidMount() {
-    axios.get('http://localhost:3333/plants')
-      .then(res => {
-        console.log(res);
+    axios
+    .get('http://localhost:3333/plants')
+    .then(res => {
+      console.log(res);
+      this.setState({
+        ...this.state, plants: res.data
+      })
       });
   }
 
